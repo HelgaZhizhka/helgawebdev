3442cb7de43fb34f03783296d3ea17a9a57bca70
Olga Zhyzhka <helgajijika@gmail.com>
2022-10-01 01:08:09 +0300

    first commit

---
+++ public/js/app.js
@@ -0,0 +1,15 @@
+/******/ (function() { // webpackBootstrap
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other entry modules.
+!function() {
+console.log('mounted app');
+}();
+// This entry need to be wrapped in an IIFE because it need to be in strict mode.
+!function() {
+"use strict";
+// extracted by mini-css-extract-plugin
+
+}();
+/******/ })()
+;
+//# sourceMappingURL=app.js.map
